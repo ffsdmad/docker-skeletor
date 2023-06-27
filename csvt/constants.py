ba6d8096be1b1b9b589ee662a0ce140f5cfe88e3
Басманов Илья <ffsdmad@gmail.com>
2023-06-27 18:24:22 +0300

    add

---
+++ csvt/constants.py
@@ -0,0 +1,28 @@
+MENU_CLASSES = ()
+ICON_CLASSES = ()
+LAYER_CLASSES = ()
+
+
+SUBPRODUCT_TYPES = (
+    ("power", "Источники питания"),
+    ("control", "Контроллеры"),
+    ("lenta", "Лента"),
+    ("nakladka", "Накладки FOTON"),
+    ("profile", "Профиль"),
+    ("ramka", "Рамки / накладки"),
+    ("lamp", "Светильники"),
+    ("chair", "Стулья"),
+    ("track", "Треки"),
+    (None, "Другое"),
+)
+
+PRODUCT_FLAGS = (
+    ("draw_first", "draw_first"),
+    ("show_trigger", "show_trigger"),
+    ("is_vivod", "is_vivod"),
+    ("add_to_cart", "add_to_cart"),
+    ("custom_params", "custom_params"),
+    ("show_category_color", "показывать цвет в карточке продукта"),
+    ("is_main_sub", "is_main_sub"),
+    ("is_dependent", "is_dependent"),
+)
