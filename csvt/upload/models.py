47c0a68b286241720af6d37f61416d70a35407f9
Басманов Илья <ffsdmad@yandex.ru>
2023-07-03 19:48:03 +0300

    fix

--- csvt/upload/models.py
+++ csvt/upload/models.py
@@ -1,3 +1,4 @@
+import hashlib
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.postgres.fields import ArrayField
@@ -14,11 +15,12 @@ class Image(TranslatableModel):
         alt = models.CharField(max_length=50)
     )
 
-    md5hash = models.CharField(max_length=32)
+    md5hash = models.CharField(max_length=32, editable=False)
 
     tags = ArrayField(
-        models.CharField(max_length=40, blank=True, choices=TAG_UPLOADS),
+        models.CharField(max_length=40, choices=TAG_UPLOADS),
         default=list,
+        blank=True,
         verbose_name=_("Tags"), size=10
     )
 
@@ -29,3 +31,13 @@ class Image(TranslatableModel):
         verbose_name = _("Images")
         verbose_name_plural = _("Images")
 
+    def calc_md5(self, file):
+        md5 = hashlib.md5()
+        for chunk in file.chunks():
+            md5.update(chunk)
+        return md5.hexdigest()
+
+    def save(self, *args, **kwargs):
+        if not self.pk or not self.md5hash:
+            self.md5hash = self.calc_md5(self.file)
+        super().save(*args, **kwargs)
